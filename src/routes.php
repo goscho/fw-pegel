9bcc9e0e4ddd45616152a2025e4002ea8c8ba3e5
ingo <code@ingoschock.de>
2026-02-22 13:07:52 +0000

    refactor: group routes to set content type json for all API responses

--- src/routes.php
+++ src/routes.php
@@ -3,10 +3,17 @@
 use App\Controller\ApiController;
 use App\Controller\ViewController;
 use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as RouteCollectorProxy;
+use App\Middleware\AddJsonResponseHeader;
 
 return function (App $app): void {
-    // Define routes
-    $app->get('/', [ViewController::class, 'index']);
-    
-    $app->get('/pegel', [ApiController::class, 'getAll']);
+    // UI routes
+    $app->group('', function (RouteCollectorProxy $group) {
+        $group->get('/', [ViewController::class, 'index']);
+    });
+
+    // API routes
+    $app->group('/api', function (RouteCollectorProxy $group) {
+        $group->get('/pegel', [ApiController::class, 'getLatest']);
+    })->add(AddJsonResponseHeader::class);
 };
